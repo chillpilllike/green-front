a587c3adcc172d2549a772597844ed6f5d7776a0
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-10-19 18:22:57 +0530

    Update index.tsx

--- src/modules/layout/templates/footer/index.tsx
+++ src/modules/layout/templates/footer/index.tsx
@@ -1,175 +1,214 @@
 import { Text, clx } from "@medusajs/ui"
-
 import { getCategoriesList, getCollectionsList } from "@lib/data"
-
 import LocalizedClientLink from "@modules/common/components/localized-client-link"
 import MedusaCTA from "@modules/layout/components/medusa-cta"
+import Head from "next/head" // Import next/head to add scripts
 
 export default async function Footer() {
   const { collections } = await getCollectionsList(0, 6)
   const { product_categories } = await getCategoriesList(0, 6)
 
   return (
-    <footer className="border-t border-ui-border-base w-full">
-      <div className="content-container flex flex-col w-full">
-        <div className="flex flex-col gap-y-6 xsmall:flex-row items-start justify-between py-40">
-          <div className="max-w-xs">
-            <LocalizedClientLink
-              href="/"
-              className="txt-compact-xlarge-plus text-ui-fg-subtle hover:text-ui-fg-base uppercase"
-            >
-              About SecretGreen
-            </LocalizedClientLink>
+    <>
+      {/* Insert external Reamaze script in the Head */}
+      <Head>
+        <script
+          type="text/javascript"
+          async
+          src="https://cdn.reamaze.com/assets/reamaze.js"
+        ></script>
+        {/* Inline script for configuring Reamaze widget */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var _support = _support || { 'ui': {}, 'user': {} };
+              _support['account'] = 'secretgreen';
+              _support['ui']['contactMode'] = 'mixed';
+              _support['ui']['enableKb'] = 'true';
+              _support['ui']['styles'] = {
+                widgetColor: 'rgba(16, 162, 197, 1)',
+                gradient: true,
+              };
+              _support['ui']['shoutboxFacesMode'] = 'brand-avatar';
+              _support['ui']['shoutboxHeaderLogo'] = true;
+              _support['ui']['widget'] = {
+                displayOn: 'all',
+                fontSize: 'default',
+                allowBotProcessing: true,
+                slug: 'secretgreen-au-chat-slash-contact-form-shoutbox',
+                label: {
+                  text: 'Let us know if you have any questions! 😊',
+                  mode: 'notification',
+                  delay: 3,
+                  duration: 30,
+                  primary: 'I have a question',
+                  secondary: 'No, thanks',
+                  sound: true,
+                },
+                position: 'bottom-right',
+                mobilePosition: 'bottom-right',
+              };
+              _support['apps'] = {
+                faq: { enabled: true },
+                recentConversations: {},
+                orders: {},
+                shopper: {},
+              };
+            `,
+          }}
+        ></script>
+      </Head>
 
-            {/* New text added just below the "Medusa Store" link with a max width to prevent overflow */}
-            <p className="mt-2 text-ui-fg-subtle text-sm">
-              Welcome to our store, your go-to destination for high-quality, unique products curated with care. We pride ourselves on delivering exceptional customer service, fast shipping, and a seamless shopping experience.
-            </p>
-            <p className="mt-2 text-ui-fg-subtle text-sm">
-              Email: support@SecretGreen.com.au
-            </p>
-          </div>
-          <div className="text-small-regular gap-10 md:gap-x-16 grid grid-cols-2 sm:grid-cols-3">
-            {product_categories && product_categories?.length > 0 && (
-              <div className="flex flex-col gap-y-2">
-                <span className="txt-small-plus txt-ui-fg-base">Categories</span>
-                <ul className="grid grid-cols-1 gap-2" data-testid="footer-categories">
-                  {product_categories?.slice(0, 6).map((c) => {
-                    if (c.parent_category) {
-                      return
-                    }
+      <footer className="border-t border-ui-border-base w-full">
+        <div className="content-container flex flex-col w-full">
+          <div className="flex flex-col gap-y-6 xsmall:flex-row items-start justify-between py-40">
+            <div className="max-w-xs">
+              <LocalizedClientLink
+                href="/"
+                className="txt-compact-xlarge-plus text-ui-fg-subtle hover:text-ui-fg-base uppercase"
+              >
+                About SecretGreen
+              </LocalizedClientLink>
 
-                    const children = c.category_children?.map((child) => ({
-                      name: child.name,
-                      handle: child.handle,
-                      id: child.id,
-                    })) || null
+              {/* New text added just below the "Medusa Store" link with a max width to prevent overflow */}
+              <p className="mt-2 text-ui-fg-subtle text-sm">
+                Welcome to our store, your go-to destination for high-quality, unique products curated with care. We pride ourselves on delivering exceptional customer service, fast shipping, and a seamless shopping experience.
+              </p>
+              <p className="mt-2 text-ui-fg-subtle text-sm">
+                Email: support@SecretGreen.com.au
+              </p>
+            </div>
+            <div className="text-small-regular gap-10 md:gap-x-16 grid grid-cols-2 sm:grid-cols-3">
+              {product_categories && product_categories?.length > 0 && (
+                <div className="flex flex-col gap-y-2">
+                  <span className="txt-small-plus txt-ui-fg-base">Categories</span>
+                  <ul className="grid grid-cols-1 gap-2" data-testid="footer-categories">
+                    {product_categories?.slice(0, 6).map((c) => {
+                      if (c.parent_category) {
+                        return;
+                      }
 
-                    return (
-                      <li className="flex flex-col gap-2 text-ui-fg-subtle txt-small" key={c.id}>
-                        <LocalizedClientLink
-                          className={clx(
-                            "hover:text-ui-fg-base",
-                            children && "txt-small-plus"
+                      const children = c.category_children?.map((child) => ({
+                        name: child.name,
+                        handle: child.handle,
+                        id: child.id,
+                      })) || null;
+
+                      return (
+                        <li className="flex flex-col gap-2 text-ui-fg-subtle txt-small" key={c.id}>
+                          <LocalizedClientLink
+                            className={clx("hover:text-ui-fg-base", children && "txt-small-plus")}
+                            href={`/categories/${c.handle}`}
+                            data-testid="category-link"
+                          >
+                            {c.name}
+                          </LocalizedClientLink>
+                          {children && (
+                            <ul className="grid grid-cols-1 ml-3 gap-2">
+                              {children.map((child) => (
+                                <li key={child.id}>
+                                  <LocalizedClientLink
+                                    className="hover:text-ui-fg-base"
+                                    href={`/categories/${child.handle}`}
+                                    data-testid="category-link"
+                                  >
+                                    {child.name}
+                                  </LocalizedClientLink>
+                                </li>
+                              ))}
+                            </ul>
                           )}
-                          href={`/categories/${c.handle}`}
-                          data-testid="category-link"
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              )}
+              {collections && collections.length > 0 && (
+                <div className="flex flex-col gap-y-2">
+                  <span className="txt-small-plus txt-ui-fg-base">Collections</span>
+                  <ul className={clx("grid grid-cols-1 gap-2 text-ui-fg-subtle txt-small", {
+                    "grid-cols-2": (collections?.length || 0) > 3,
+                  })}>
+                    {collections?.slice(0, 6).map((c) => (
+                      <li key={c.id}>
+                        <LocalizedClientLink
+                          className="hover:text-ui-fg-base"
+                          href={`/collections/${c.handle}`}
                         >
-                          {c.name}
+                          {c.title}
                         </LocalizedClientLink>
-                        {children && (
-                          <ul className="grid grid-cols-1 ml-3 gap-2">
-                            {children.map((child) => (
-                              <li key={child.id}>
-                                <LocalizedClientLink
-                                  className="hover:text-ui-fg-base"
-                                  href={`/categories/${child.handle}`}
-                                  data-testid="category-link"
-                                >
-                                  {child.name}
-                                </LocalizedClientLink>
-                              </li>
-                            ))}
-                          </ul>
-                        )}
                       </li>
-                    )
-                  })}
-                </ul>
-              </div>
-            )}
-            {collections && collections.length > 0 && (
+                    ))}
+                  </ul>
+                </div>
+              )}
               <div className="flex flex-col gap-y-2">
-                <span className="txt-small-plus txt-ui-fg-base">Collections</span>
-                <ul className={clx("grid grid-cols-1 gap-2 text-ui-fg-subtle txt-small", {
-                  "grid-cols-2": (collections?.length || 0) > 3,
-                })}>
-                  {collections?.slice(0, 6).map((c) => (
-                    <li key={c.id}>
-                      <LocalizedClientLink
-                        className="hover:text-ui-fg-base"
-                        href={`/collections/${c.handle}`}
-                      >
-                        {c.title}
-                      </LocalizedClientLink>
-                    </li>
-                  ))}
+                <span className="txt-small-plus txt-ui-fg-base">Useful info.</span>
+                <ul className="grid grid-cols-1 gap-y-2 text-ui-fg-subtle txt-small">
+                  <li>
+                    <a
+                      href="/privacy-policy"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="hover:text-ui-fg-base"
+                    >
+                      Privacy policy
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/terms-conditions"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="hover:text-ui-fg-base"
+                    >
+                      Terms & Conditions
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/refund-policy"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="hover:text-ui-fg-base"
+                    >
+                      Refund Policy
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/shipping-policy"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="hover:text-ui-fg-base"
+                    >
+                      Shipping Policy
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/au/contact-us"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="hover:text-ui-fg-base"
+                    >
+                      Contact us
+                    </a>
+                  </li>
                 </ul>
               </div>
-            )}
-            <div className="flex flex-col gap-y-2">
-              <span className="txt-small-plus txt-ui-fg-base">Useful info.</span>
-              <ul className="grid grid-cols-1 gap-y-2 text-ui-fg-subtle txt-small">
-                <li>
-                  <a
-                    href="/privacy-policy"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="hover:text-ui-fg-base"
-                  >
-                    Privacy policy
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/terms-conditions"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="hover:text-ui-fg-base"
-                  >
-                    Terms & Conditions
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/refund-policy"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="hover:text-ui-fg-base"
-                  >
-                    Refund Policy
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/shipping-policy"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="hover:text-ui-fg-base"
-                  >
-                    Shipping Policy
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/au/contact-us"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="hover:text-ui-fg-base"
-                  >
-                    Contact us
-                  </a>
-                </li>
-              </ul>
             </div>
           </div>
+          <div className="flex w-full mb-16 justify-between text-ui-fg-muted">
+            <Text className="txt-compact-small">
+              © {new Date().getFullYear()} SecretGreen.com.au All rights reserved.
+            </Text>
+            <MedusaCTA />
+          </div>
         </div>
-        <div className="flex w-full mb-16 justify-between text-ui-fg-muted">
-          <Text className="txt-compact-small">
-            © {new Date().getFullYear()} SecretGreen.com.au All rights reserved.
-          </Text>
-          <MedusaCTA />
-        </div>
-      </div>
-
-      {/* Reamaze Base Installation Script */}
-      <script type="text/javascript" async src="https://cdn.reamaze.com/assets/reamaze.js"></script>
-
-      {/* Reamaze Brand Identification Script */}
-      <script type="text/javascript">
-        var _support = _support || { 'ui': {}, 'user': {} };
-        _support['account'] = '{{ secretgreen }}';
-      </script>
-    </footer>
+      </footer>
+    </>
   )
 }
