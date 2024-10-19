46b3f250e94365093aaf09fdc2b6102ad181075c
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-10-20 00:37:37 +0530

    Update index.tsx

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -1,9 +1,14 @@
-import { Github } from "@medusajs/icons"
-import { Button, Heading } from "@medusajs/ui"
+import { Github } from "@medusajs/icons";
+import { Button, Heading } from "@medusajs/ui";
 
 const Hero = () => {
   return (
-    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
+    <div
+      className="h-[75vh] w-full border-b border-ui-border-base relative bg-cover bg-center"
+      style={{
+        backgroundImage: "url('https://img.freepik.com/free-photo/beautiful-scenery-summit-mount-everest-covered-with-snow-white-clouds_181624-21317.jpg')", // Add the background image here
+      }}
+    >
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
         <span>
           <Heading
@@ -19,17 +24,14 @@ const Hero = () => {
             Free Shipping | 30 Days Return | Secure payments
           </Heading>
         </span>
-        <a
-          href="/store"
-          target="_blank"
-        >
+        <a href="/store" target="_blank">
           <Button variant="secondary">
             Shop the Latest Trends!
-            </Button>
+          </Button>
         </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
